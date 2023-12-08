7e0d9a1cc8f575e972f5cf78a62317aa25f6f478
alvesluis311 <felipe7311141@gmail.com>
2023-12-08 06:53:56 -0300

    cadastro

--- angular-topicos2-2023-2/src/app/auth/auth-routing.module.ts
+++ angular-topicos2-2023-2/src/app/auth/auth-routing.module.ts
@@ -1,11 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
+import { CadastroComponent } from './components/cadastro/cadastro.component';
 
 const routes: Routes = [
   {
     path: 'login',
     component: LoginComponent,
+  },{
+    path: 'cadastro',
+    component: CadastroComponent,
   },
   // Outras rotas, se necessário
 ];
--- angular-topicos2-2023-2/src/app/auth/auth.module.ts
+++ angular-topicos2-2023-2/src/app/auth/auth.module.ts
@@ -5,15 +5,25 @@ import { AuthRoutingModule } from './auth-routing.module';
 import { LoginComponent } from './components/login/login.component';
 import { MatCardModule } from '@angular/material/card';
 import { MatFormFieldModule } from '@angular/material/form-field';
-import { ReactiveFormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+import { CadastroComponent } from './components/cadastro/cadastro.component';
+import { UsuarioRoutingModule } from '../usuario/usuario-routing.module';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatTableModule } from '@angular/material/table';
+import { MatSelectModule } from '@angular/material/select';
+import { MatListModule } from '@angular/material/list';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatChipsModule } from '@angular/material/chips';
+import { MatPaginatorModule } from '@angular/material/paginator';
 
 
 @NgModule({
   declarations: [
-    LoginComponent
+    LoginComponent,
+    CadastroComponent
   ],
   imports: [
     CommonModule,
@@ -23,7 +33,16 @@ import { MatIconModule } from '@angular/material/icon';
     ReactiveFormsModule,
     MatInputModule,
     MatButtonModule,
-    MatIconModule
+    MatIconModule,
+    CommonModule,
+    MatToolbarModule,
+    MatTableModule,
+    MatSelectModule,
+    MatListModule,
+    MatDialogModule,
+    MatChipsModule,
+    MatPaginatorModule,
+    FormsModule
   ]
 })
 export class AuthModule { }
