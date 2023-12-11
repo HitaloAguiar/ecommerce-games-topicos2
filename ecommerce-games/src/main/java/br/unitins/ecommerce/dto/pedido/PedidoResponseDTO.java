cb037ebea3d175212a5c1fbc0baa580431067009
alvesluis311 <felipe7311141@gmail.com>
2023-12-10 22:12:54 -0300

    Revert "Merge branch 'master' of https://github.com/HitaloAguiar/ecommerce-games-topicos2"
    This reverts commit 82ca4a4a2c7bd81bc69ec949d3758370a3b93175, reversing
    changes made to 12bd0f9a1444f2459f185255fded4f8073551f54.

--- ecommerce-games/src/main/java/br/unitins/ecommerce/dto/pedido/PedidoResponseDTO.java
+++ ecommerce-games/src/main/java/br/unitins/ecommerce/dto/pedido/PedidoResponseDTO.java
@@ -1,46 +1,22 @@
 package br.unitins.ecommerce.dto.pedido;
 
-import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.List;
 
-import br.unitins.ecommerce.dto.endereco.EnderecoResponseDTO;
 import br.unitins.ecommerce.dto.itempedido.ItemPedidoResponseDTO;
 import br.unitins.ecommerce.dto.usuario.UsuarioResponseDTO;
-import br.unitins.ecommerce.model.pedido.Pedido;
-import br.unitins.ecommerce.model.pedido.pagamento.BoletoBancario;
-import br.unitins.ecommerce.model.pedido.pagamento.Pix;
+import br.unitins.ecommerce.model.Pedido;
 
 public record PedidoResponseDTO(
     Long id,
-    LocalDateTime dataHoraPedido,
-    String totalPedido,
-    String formaPagamento,
-    String statusPagamento,
-    LocalDate dataPagamento,
-    String statusPedido,
-    EnderecoResponseDTO enderecoResponseDTO,
+    LocalDateTime dataHora,
     UsuarioResponseDTO usuario,
+    Double totalPedido,
     List<ItemPedidoResponseDTO> itens
 ) {
     
     public PedidoResponseDTO (Pedido pedido) {
 
-        this(pedido.getId(),
-            pedido.getDataHoraPedido(),
-            "R$ " + String.format("%.2f", pedido.getTotalPedido()),
-            pedido.getPagamento() instanceof Pix ? "Pix" : pedido.getPagamento() instanceof BoletoBancario ? "Boleto Bancário" : "Cartão de Crédito",
-            pedido.getPagamento() != null?
-                pedido.getPagamento().getConfirmacaoPagamento() == true?
-                    "Pagamento efetuado":
-                    "Pagamento não efetuado" :
-            null,
-            pedido.getPagamento() != null ?
-                pedido.getPagamento().getDataConfirmacaoPagamento() :
-                null,
-            pedido.getIfConcluida() == true ? "Compra concluída" : "Compra em andamento",
-            new EnderecoResponseDTO(pedido.getEndereco(), pedido.getUsuario().getLogin()),
-            new UsuarioResponseDTO(pedido.getUsuario(), pedido.getUsuario().getPerfil().getLabel()),
-            ItemPedidoResponseDTO.valueOf(pedido.getItens()));
+        this(pedido.getId(), pedido.getDataHora(), new UsuarioResponseDTO(pedido.getUsuario(), pedido.getUsuario().getPerfil().getLabel()), pedido.getTotalPedido(), ItemPedidoResponseDTO.valueOf(pedido.getItens()));
     }
 }
